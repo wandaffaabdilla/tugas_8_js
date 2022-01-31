ca78a96603ec80f901148b9c8002c32faa37e2de
wan daffa abdilla <wandaffaadilla@gmail.com>
2022-02-01 01:27:57 +0800

    Tugas 8 JS
    Tugas ini kami kumpulkan untuk niomic

---
+++ tugas8.js
@@ -0,0 +1,12 @@
+function pemanggilObjek() {
+  var motor = {
+    type: "moge",
+    cc: "600cc",
+    silinder: "4 silinder",
+    warna: "hitam doff",
+    pemilik: "abdul",
+  };
+  motor.pemilik = "udin";
+  console.log(motor);
+}
+pemanggilObjek();
